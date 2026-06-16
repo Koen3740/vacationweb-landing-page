31b890734c4d52d6d07ded78ca39a8053662ae7b
Koen3740 <koen.munsters@hotmail.com>
2026-06-16 23:43:46 +0200

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,1 @@
-// app/page.tsx
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Welkom bij VacationWeb</h1>
-      <p>Vind de beste vakanties voor jouw budget!</p>
-    </div>
-  )
-}
+export default function Home() { return <h1>Welkom bij VacationWeb</h1>; }
