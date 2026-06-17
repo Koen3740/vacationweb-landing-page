87e5941b434a2630c0445db3a9f51b48ce1da41f
Koen3740 <koen.munsters@hotmail.com>
2026-06-17 16:27:53 +0200

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
-export default function Home() { return <h1>Welkom bij VacationWeb</h1>; }
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Meer vakantie voor jouw budget</h1>
+      <p>Traditionele reiswebsites vertrekken vanuit een bestemming en vertrekdatum. Bij ons vergelijk je meerdere opties.</p>
+    </main>
+  )
+}
